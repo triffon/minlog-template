144ca046c0eb5aecb7be442cc0714f20eade7329
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2023-05-11 14:46:13 +0300

    Опростена поддръжка на свободни променливи в NbE

--- nbe.scm
+++ nbe.scm
@@ -25,45 +25,6 @@
 (define base? var?)
 (define arrow? pair?)
 
-(define (unionv l1 l2)
-  (cond ((null? l1) l2)
-        ((memv (car l1) l2) (unionv (cdr l1) l2))
-        (else (cons (car l1) (unionv (cdr l1) l2)))))
-
-(define (fv M)
-  (cond ((var? M) (list M))
-        ((app? M) (unionv (fv (get-fun M))
-                          (fv (get-arg M))))
-        ((abs? M) (filter
-                   (lambda (x)
-                     (not (eqv? x (get-var M))))
-                   (fv (get-body M))))))
-
-(define (make-λs M vars)
-  (foldr make-λ M vars))
-
-(define (make-arrows ρs σ)
-  (foldr make-arrow σ ρs)) 
-
-(define (close M)
-  (make-λs M (fv M)))
-
-(define (rename-var M x y)
-  (cond ((and (var? M) (eqv? M x) y))
-        ((var? M) M)
-        ((app? M) (make-app
-                   (rename-var (get-fun M) x y)
-                   (rename-var (get-arg M) x y)))
-        ((abs? M) (make-λ
-                   (get-var M)
-                   (rename-var (get-body M) x y)))))
-
-(define (reduce-vars xs M)
-  (if (null? xs) M
-      (rename-var
-       (reduce-vars (cdr xs) (get-body M))
-       (get-var M) (car xs))))
-  
 ;; оценките ще са функции от променливи в стойности
 
 (define (modify ξ x a)
@@ -96,14 +57,10 @@
         (make-λ x
                   (⇓ σ (a (⇑ ρ x)))))))
 
-;; ((x₁ τ₁) (x₂ τ₂) ... (xₙ τₙ)) - контекст 
+;; ((x₁ τ₁) (x₂ τ₂) ... (xₙ τₙ)) - контекст
 
 (define (nbe Γ τ M)
-  (let ((xs (map car Γ))
-        (ρs (map cadr Γ)))
-    (reduce-vars xs
-                 (⇓ (make-arrows ρs τ)
-                    (evaluate (make-λs M xs) 'empty)))))
+  (⇓ τ (evaluate M (lambda (x) (⇑ (cadr (assv x Γ)) x)))))
 
 (define S '(λ (x) (λ (y) (λ (z) ((x z) (y z))))))
 (define K '(λ (x) (λ (y) x)))
@@ -127,4 +84,4 @@
                                   (make-app (make-app 'm 'f) (make-app (make-app 'n 'f) 'x)))))))
 
 (define tn `(⇒ ,ti ,ti))
-(define c13 (make-app (make-app c+ (c 5)) (c 8)))
+(define c13 (nbe '() tn (make-app (make-app c+ (c 5)) (c 8))))
