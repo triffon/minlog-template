74a4ebf61fab0f22139b667e51e7743236ff429b
Trifon Trifonov <triffon@fmi.uni-sofia.bg>
2023-06-01 09:12:13 +0300

    Added Minlog cheatsheet

--- example.rkt
+++ example.rkt
@@ -1,4 +1,41 @@
 #lang r5rs
 
+;; py = parse type
+;; pt = parse term
+;; pf = parse formula
+;; pp = pretty print
+
+;; (add-pvar-name <name> <arity>)
+;; create a new predicate variable (symbol) with <name> and <arity>
+
+;; (make-arity {<types>})
+;; create a new "arity" object with the specified types
+
+;; -> = →
+;; & = ∧
+;; ord = ∨
+;; not = ¬
+;; all = ∀
+;; ex = ∃
+
+;; (set-goal <formula>)
+;; start a new proof with <formula> as a goal
+
+;; (assume {<avar> | <var>}⁺)
+;; →⁺ and ∀⁺ with <avar>s or <var>s
+
+;; (use {<avar> | <term>}⁺)
+;; →⁻ and ∀⁻ with assumption(s) <avar> and term(s) <term>
+
+;; (split) = ∧⁺
+;; (intro <n>) = ∨⁺ₙ
+;; (elim) = ∨⁻
+;; (ord-intro [01])
+;; (ex-intro <term>) = ∃⁺ <term>
+;; (ex-elim <avar>) = ∃⁻ <avar>
+
+;; (add-var-name <name> <type>)
+;; create a new variable with <name> of <type>
+
 (add-pvar-name "A" (make-arity))
 (set-goal (pf "A -> A"))
